9686d69f5d59e79cb4536679ad0f9efa6fac724b
BataevDaniil <ragoga48@gmail.com>
2019-04-17 17:09:56 +0300

    Add handle not loaded webview

--- app/src/main/java/codemonx/com/appinappphotosandroid/AppinappPhotosAndroid.java
+++ app/src/main/java/codemonx/com/appinappphotosandroid/AppinappPhotosAndroid.java
@@ -1,13 +1,18 @@
 package codemonx.com.appinappphotosandroid;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class AppinappPhotosAndroid extends AppCompatActivity {
-    static String baseUrl = "http://dev-appinapp-photos-app.s3-website-us-east-1.amazonaws.com/?apiKey=";
+    private static String baseUrl = "http://dev-appinapp-photos-app.s3-website-us-east-1.amazonaws.com/?apiKey=";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +23,7 @@ public class AppinappPhotosAndroid extends AppCompatActivity {
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         webSettings.setDomStorageEnabled(true);
+        webView.setWebViewClient(new ViewClient());
 
         Bundle info = getIntent().getExtras();
         String url = info.getString("apiKey");
@@ -26,6 +32,21 @@ public class AppinappPhotosAndroid extends AppCompatActivity {
             Log.e("AppinappPhotosAndroid", "url unspecified in bundle");
         } else {
             webView.loadUrl(AppinappPhotosAndroid.baseUrl + url);
+        }
+    }
+
+    public class ViewClient extends WebViewClient {
+        public void onReceivedError (WebView view, WebResourceRequest request, WebResourceError error) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(AppinappPhotosAndroid.this);
+            builder.setTitle("No Internet connection. Please try again later!")
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            AppinappPhotosAndroid.this.finish();
+                            dialog.cancel();
+                        }
+                    });
+            AlertDialog alert = builder.create();
+            alert.show();
         }
     }
 }
